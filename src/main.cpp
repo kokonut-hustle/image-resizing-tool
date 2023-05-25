e188a0805c7f4bb9652a7a88e6f663a7dbbfbdbb
trung.dang <kokonut.hustle@gmail.com>
2023-05-25 15:00:41 +0700

    add build files

---
+++ src/main.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+#include <constants.hpp>
+
+int main(int argc, char *argv[]) {
+    std::cout << "Hello World" << std::endl;
+    std::cout << "This is test constant " << TEST_CONSTANT << std::endl;
+    return 0;
+}
