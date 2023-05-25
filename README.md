b85411bdfc024f221aa9a900d52903de55f0d14a
trung.dang <kokonut.hustle@gmail.com>
2023-05-25 16:12:17 +0700

    add stb_image lib

---
+++ README.md
@@ -0,0 +1,3 @@
+## Image Resizing Tool
+### Dependencies
+<a href="https://github.com/nothings/stb">stb_image</a> for loading and writing images.
--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,20 @@
 #include <iostream>
 #include <constants.hpp>
 
+#define STB_IMAGE_IMPLEMENTATION
+#include <stb_image.h>
+#define STB_IMAGE_WRITE_IMPLEMENTATION
+#include <stb_image_write.h>
+
 int main(int argc, char *argv[]) {
-    std::cout << "Hello World" << std::endl;
-    std::cout << "This is test constant " << TEST_CONSTANT << std::endl;
+    int width, height, channels;
+    unsigned char *img = stbi_load("image/test.jpg", &width, &height, &channels, 0);
+    if (img == NULL) {
+        std::cout << "Error in loading the image" << std::endl;
+        exit(1);
+    }
+    std::cout << "Loaded image with a width of " << width << " dpx, a height of "
+                << height << " dpx and " << channels << " channels" << std::endl;
+
     return 0;
 }
